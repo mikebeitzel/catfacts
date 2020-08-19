d81e8024dc90b64b1a7b64d94f1e6f6d73b6c704
Mike <mikebeitzel@gmail.com>
2020-08-19 00:59:09 -0500

    Basic Cat Fact Run

--- app/src/main/java/com/catfacts/MainActivity.kt
+++ app/src/main/java/com/catfacts/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.catfacts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mTextViewResult: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTextViewResult = findViewById(R.id.catFactText)
+
+        val client = OkHttpClient()
+        val url = "https://catfact.ninja/fact?max_length=140"
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        catFactButton.setOnClickListener {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: java.io.IOException) {
+                    e.printStackTrace()
+                }
+
+                @Throws(java.io.IOException::class)
+
+                override fun onResponse(
+                    call: Call,
+                    response: Response
+                ) {
+                    if (response.isSuccessful) {
+                        val myResponse = response.body!!.string()
+                        runOnUiThread { mTextViewResult?.run { setText(myResponse) } }
+                    }
+                }
+            })
+        }
     }
 }
