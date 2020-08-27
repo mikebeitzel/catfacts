05915af757b5c2162a17a15031544df97e43f33b
Mike <mikebeitzel@gmail.com>
2020-08-26 20:44:07 -0500

    Building The Layouts

---
+++ app/src/main/java/com/catfacts/CatFactsButton.kt
@@ -0,0 +1,57 @@
+package com.catfacts
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
+
+class CatFactsButton : AppCompatActivity() {
+
+    private var mTextViewResult: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mTextViewResult = findViewById(R.id.catFactText)
+
+        val client = OkHttpClient()
+        val url = "https://catfact.ninja/fact?max_length=140"
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        catFactButton.setOnClickListener {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: java.io.IOException) {
+                    e.printStackTrace()
+                }
+
+                @Throws(java.io.IOException::class)
+
+                override fun onResponse(
+                    call: Call,
+                    response: Response
+                ) {
+                    if (response.isSuccessful) {
+                        val responseData = response.body!!.string()
+                        runOnUiThread {
+                            try {
+                                val json = JSONObject(responseData)
+                                println("Request Successful!!")
+                                println(json)
+                                val responseObject = json.getString("fact")
+                                catFactText.text = responseObject
+                            } catch (e: JSONException) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+}
--- app/src/main/java/com/catfacts/NavigationActivity.kt
+++ app/src/main/java/com/catfacts/NavigationActivity.kt
@@ -1,13 +1,11 @@
 package com.catfacts
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-
+        setContentView(R.layout.activity_navigation)
     }
 }
